1bc61e0aefa9e5dc13d3af8788ada3995e0b8cfa
CodreaCodrin <68387109+CodreaCodrin@users.noreply.github.com>
2022-11-21 20:42:47 +0200

    Fix

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -11,27 +11,28 @@ public class PlayerCombat : MonoBehaviour
     public LayerMask enemyLayers;
 
     public float attackRate = 2f;
-    private float nextAttackTime = 0f;
+    float nextAttackTime = 0f;
     
     // Update is called once per frame
     void Update()
     {
         if(Time.time >= nextAttackTime)
         {
-             if(Input.GetKeyDown(KeyCode.E))
-                    {
-                        Attack();
-                nextAttackTime = Time.time + 1f / attackRange;
-                    }
+            if(Input.GetMouseButtonDown(0))
+            {
+                if (animator.GetBool("IsJumping") == false)
+                {
+                    Attack();
+                    nextAttackTime = Time.time + 1f / attackRate;
+                }
+            }
         }
-       
-       
     }
 
     void Attack()
     {
         animator.SetTrigger("Attack");
-
+            
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
         foreach(Collider2D enemy in hitEnemies)
